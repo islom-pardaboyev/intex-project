45cd92167c984108e0444ce3f95589beada54c5c
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-07-12 17:40:38 +0500

    inint

--- js/login.js
+++ js/login.js
@@ -6,26 +6,28 @@ form.addEventListener('submit', evt => {
     const username = evt.target.username.value;
     const password = evt.target.password.value;
 
-    if (username == 'islom' && password == "123") {
-        const regex = /[1-9]/;
-
-        if (regex.test(password)) {
-            const userData = {
-                username: username,
-                password: password
-            }
-            window.localStorage.setItem("userdata", JSON.stringify(userData))
-            alert("Registration successful!");
-            setTimeout(() => {
-                location.pathname = 'admin.html'
-            }, 1000);
-
-        } else {
-            alert("Password must includes number");
-        }
-
+    if (username === "" || password === "") {
+        alert("Please fill both fields");
     } else {
-        alert("Username and password are wrong.");
+        if (username !== "islom") {
+            alert("Username is wrong");
+        } else if (password !== "123") {
+            alert("Password is wrong");
+        } else {
+            const regex = /[1-9]/;
+            if (regex.test(password)) {
+                const userData = {
+                    username: username,
+                    password: password
+                };
+                window.localStorage.setItem("userdata", JSON.stringify(userData));
+                alert("Registration successful!");
+                setTimeout(() => {
+                    location.pathname = 'admin.html';
+                }, 1000);
+            } else {
+                alert("Password must include a number");
+            }
+        }
     }
-
 });
