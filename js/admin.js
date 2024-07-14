19b6d29fe5d9e59242058cd3b01533824ea6840a
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-07-14 20:50:42 +0500

    inint

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,22 @@
 document.querySelector("#username").textContent = JSON.parse(window.localStorage.getItem('userdata')).username
+let navbarList = document.querySelector('.list')
 document.querySelector("#logout").addEventListener('click', () => {
     let logOut = confirm("do you want to log out")
 
-    if(logOut){
+    if (logOut) {
         setTimeout(() => {
             location.pathname = 'index.html'
             window.localStorage.clear()
         }, 300)
+    }
+})
+
+navbarList.addEventListener('click', e => {
+    if (e.target.matches('.navbar-item1')) {
+        e.target.classList.add("!text-[#009398]", "!border-[#009398]")
+        e.target.nextElementSibling.classList.remove("!text-[#009398]", "!border-[#009398]")
+    } else if (e.target.matches(".navbar-item2")) {
+        e.target.classList.add("!text-[#009398]", "!border-[#009398]")
+        e.target.previousElementSibling.classList.remove("!text-[#009398]", "!border-[#009398]")
     }
 })
